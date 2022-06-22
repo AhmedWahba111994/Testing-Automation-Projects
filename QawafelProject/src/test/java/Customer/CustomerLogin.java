8af5ca97550a97504607ad60639db02ab2f4079f
AhmedWahba <63453899+AhmedWahba111994@users.noreply.github.com>
2022-06-22 13:53:57 +0200

    Qawafel Project API Testing

---
+++ QawafelProject/src/test/java/Customer/CustomerLogin.java
@@ -0,0 +1,21 @@
+package Customer;
+
+
+
+
+import com.shaft.api.*;
+import com.shaft.driver.*;
+import org.testng.annotations.Test;
+
+import static com.shaft.api.RestActions.*;
+import static io.restassured.RestAssured.*;
+
+public class CustomerLogin {
+
+    SHAFT.API driver;
+
+    @Test
+    public void test(){
+
+    }
+}
